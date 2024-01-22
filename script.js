9e5c9db7e6de90d2474249f3851332b7cf855373
Torgeir Hodne-Høgheim <hogheim@gmail.com>
2024-01-22 12:30:15 +0100

    AttackDragon() og dragonAttack() - funksjoner

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
  document.querySelector('.healer').addEventListener('click', function (){
    attackDragon(heroesArray[0]);
+   console.log("Du klikket på Henriette")
  })
  document.querySelector('.archer').addEventListener('click', function(){
    attackDragon(heroesArray[1]);
@@ -43,3 +44,49 @@
    damage: 200,
    alive: true,
  };
+
+ function attackDragon(hero) {
+   if (dragonObject.alive && hero.alive) {
+     dragonObject.currentHP -= hero.damage;
+     console.log(`${hero.name} har gjort ${hero.damage} skade på ${dragonObject.name}!`);
+     updateHealthBar(dragonObject);
+ 
+     if (dragonObject.currentHP <= 0) {
+       dragonObject.alive = false;
+       dragonObject.currentHP = 0;
+       updateHealthBar(dragonObject);
+       alert(`Gratulerer! Du har beseiret dragen og vunnet spillet!`);
+     } else {
+       dragonAttacks(); // Kall dragenAttacks for motangrep
+     }
+   }
+ }
+
+ function updateHealthBar(){
+   
+ }
+ 
+
+ function dragonAttacks(){
+   if(dragonObject.alive){
+      let livingHeroes = heroesArray.filter(hero => hero.alive);
+      if (livingHeroes.length > 0) {
+         let randomIndex = Math.floor(Math.random() * livingHeroes.length);
+         let targetHero = livingHeroes[randomIndex];
+
+         //Dragen angriper tilfeldig karakter
+         console.log(`${dragonObject.name} har angrepet ${targetHero}`);
+         updateHealthBar(targetHero);
+
+         if(targetHero-currentHP <= 0){
+            targetHero.alive = false;
+            targetHero.currentHP = 0;
+            updateHealthBar(targetHero);
+            checkGameOver();
+         }
+      }
+      if(!heroesArray.some(hero => hero.alive)){
+         alert(`Spillet er tapt! ${dragonObject.name} har vunnet!`)
+      }
+   }
+ }
